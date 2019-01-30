92538f0ba56dd9edf9053ca625f1b5309348590d
AlexGruPerm <ugr@bk.ru>
2019-01-30 15:15:23 +0500

    Successful read one row with Map column.

--- src/main/scala/ScalaCassMapCol.scala
+++ src/main/scala/ScalaCassMapCol.scala
@@ -1,7 +1,10 @@
+import java.util.Date
+
 import com.datastax.driver.core.{Cluster, Row}
 import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters
+import scala.collection.JavaConverters._
 
 object ScalaCassMapCol extends App {
 
@@ -9,16 +12,26 @@ object ScalaCassMapCol extends App {
     @transient lazy val log = LoggerFactory.getLogger(getClass.getName)
   }
 
-  case class TicksCntTotalRow(ticker_id : Int, ticks_count :Long){
-    override def toString = "ticker_id="+ticker_id+"  ["+ticks_count+"]"
+  case class bars(ticker_id :Int,
+                  ddate     :java.util.Date,
+                  tdBars    :Map[String,String]){
+    override def toString = "ticker_id="+ddate+"  ["+ddate+"]" + tdBars.toString()
   }
 
-  val rowToTicksCntTotalRow = (row : Row) => {
-    new TicksCntTotalRow(
+
+
+  val rowToBars = (row : Row) => {
+    new bars(
       row.getInt("ticker_id"),
-      row.getLong("ticks_count")
+      new Date(row.getDate("ddate").getMillisSinceEpoch),
+      row.getMap("bar_1", classOf[String], classOf[String]).asScala.toMap
     )
   }
+
+  /*
+   Immutable Map myJavaMap.asScala.toMap
+   Mutable Map myJavaMap.asScala
+  */
 
   slog.log.debug("BEGIN")
   val node: String = "193.124.112.90"
@@ -28,22 +41,26 @@ object ScalaCassMapCol extends App {
   /*EXAMPLE OF SELECT.*/
 
   val queryTicksCountTotal =
-    """ select *
-          from mts_src.ticks_count_total """
+    """ select ticker_id,
+               ddate,
+               bar_1
+          from mts_bars.td_bars_3600 """
 
   val pqueryTicksCountTotal = session.prepare(queryTicksCountTotal)
 
   val bound = pqueryTicksCountTotal.bind()
 
-  val dsTicksCntTotalRow : Seq[TicksCntTotalRow] = JavaConverters.asScalaIteratorConverter(session.execute(bound).all().iterator())
+  val dsTicksCntTotalRow : Seq[bars] = JavaConverters.asScalaIteratorConverter(session.execute(bound).all().iterator())
     .asScala
-    .toSeq.map(rowToTicksCntTotalRow)
-    .sortBy(ft => ft.ticks_count)(Ordering[Long].reverse)
+    .toSeq.map(rowToBars)
+    .sortBy(ft => ft.ticker_id)(Ordering[Int].reverse)
 
+  slog.log.debug("=====================================")
   for (rowTicksCntTotalRow <- dsTicksCntTotalRow){
     slog.log.debug(rowTicksCntTotalRow.toString)
   }
-  session.close()
+  slog.log.debug("=====================================")
 
+  session.close()
 
 }
